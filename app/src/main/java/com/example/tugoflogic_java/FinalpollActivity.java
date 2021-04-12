c86a5b1135e9f093a5788fb24dd3281a58517151
Sangmin Lee <sangminhun@gmail.com>
2021-04-11 21:34:55 -0700

    finished final pages

--- app/src/main/java/com/example/tugoflogic_java/FinalpollActivity.java
+++ app/src/main/java/com/example/tugoflogic_java/FinalpollActivity.java
@@ -3,11 +3,13 @@ package com.example.tugoflogic_java;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -25,13 +27,12 @@ public class FinalpollActivity extends AppCompatActivity {
 
     private TextView tvMainClaim, tvPlayerName;
     private RadioGroup rg_finalPoll;
-    private Button btnSubmitFinal;
+    private Button btnSubmitFinal, btnShowGraph;
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference mDatabase = firebaseDatabase.getReference();
     DatabaseReference mcDB = mDatabase.child("MainClaim");
     DatabaseReference playerDB = firebaseDatabase.getReference("Player");
-    //DatabaseReference settingDB = mDatabase.child("GameSetting");
 
     FirebaseUser currentPlayer = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -52,12 +53,13 @@ public class FinalpollActivity extends AppCompatActivity {
 
         rg_finalPoll = findViewById(R.id.voteFinalPoll);
         btnSubmitFinal = findViewById(R.id.btnSubmitFinal);
+        btnShowGraph = findViewById(R.id.btnShowFinalGraph);
 
         // final voting step
         rg_finalPoll.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                if(checkedId == R.id.finalNewConvinced){
+                if(checkedId == R.id.finalStillConvinced){
                     finalResult = "Still Convinced"; // does not need to show final poll result b/c initial straw poll result is same.
                 }if(checkedId == R.id.finalNewConvinced){
                     finalResult = "Newly Convinced";
@@ -66,21 +68,49 @@ public class FinalpollActivity extends AppCompatActivity {
                 }
             }
         });
+        btnShowGraph.setVisibility(View.INVISIBLE);
 
+        // only interested in newly convinced and no longer persuaded so only show graph to two group
         btnSubmitFinal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 playerDB.child(playerKey).child("finalResult").setValue(finalResult);
                 if(finalResult == "Still Convinced"){
                     numFinalCon++;
+                    Toast.makeText(FinalpollActivity.this, "Still Convinced Submitted", Toast.LENGTH_SHORT).show();
+                    btnShowGraph.setText("Go To Comment");
                 }if(finalResult == "Newly Convinced"){
                     numFinalNew++;
+                    Toast.makeText(FinalpollActivity.this, "Newly Convinced Submitted", Toast.LENGTH_SHORT).show();
+                    btnShowGraph.setText("Show Graph");
                 }if(finalResult == "No Longer Persuaded"){
-                    numFinalCon++;
+                    numFinalNot++;
+                    Toast.makeText(FinalpollActivity.this, "No Longer Persuaded Submitted", Toast.LENGTH_SHORT).show();
+                    btnShowGraph.setText("Show Graph");
                 }
                 mcDB.child("mc").child("numFinalCon").setValue(numFinalCon);
                 mcDB.child("mc").child("numFinalNot").setValue(numFinalNot);
                 mcDB.child("mc").child("numFinalNew").setValue(numFinalNew);
+                btnSubmitFinal.setVisibility(View.INVISIBLE);
+                btnShowGraph.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnShowGraph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(finalResult == "Still Convinced") {
+                    Intent intent = new Intent(getApplicationContext(), FinalpollResultActivity.class);
+                    startActivity(intent);
+                }
+                if(finalResult == "Newly Convinced") {
+                    Intent intent = new Intent(getApplicationContext(), FinalPollGraph.class);
+                    startActivity(intent);
+                }
+                if(finalResult == "No Longer Persuaded") {
+                    Intent intent = new Intent(getApplicationContext(), FinalPollGraph.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -115,7 +145,7 @@ public class FinalpollActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 DB_Player player = null;
                 player = dataSnapshot.getValue(DB_Player.class);
-                tvPlayerName.setText("Let's Start Final Poll " + player.name + "\n");
+                tvPlayerName.setText("Final Poll, " + player.name + "\n");
             }
 
             @Override
--- app/src/main/java/com/example/tugoflogic_java/FinalpollResultActivity.java
+++ app/src/main/java/com/example/tugoflogic_java/FinalpollResultActivity.java
@@ -2,12 +2,15 @@ package com.example.tugoflogic_java;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Description;
@@ -17,6 +20,8 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,14 +37,15 @@ public class FinalpollResultActivity extends AppCompatActivity {
     private EditText finalComment;
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    DatabaseReference mainClaimDB = firebaseDatabase.getReference("MainClaim");
     DatabaseReference playerDB = firebaseDatabase.getReference("Player");
 
+    String playerKey;
 
-    ArrayList<BarEntry> entries = new ArrayList<>();
-    BarChart barChart;
-    Integer totPlayerNum;
-    ArrayList barEntries;
+    ArrayList<DB_Player> playerRipList;
+    RecyclerView recyclerView;
+    RipAdapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    DB_Player selectRip;
 
 
     @Override
@@ -47,15 +53,33 @@ public class FinalpollResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalpoll_result);
 
-        barChart = findViewById(R.id.barChartFinalResult);
+        //barChart = findViewById(R.id.barChartFinalResult);
         btnSummitCom = findViewById(R.id.btnSummitFinalCom);
+        finalComment = findViewById(R.id.editTxtFinalComment);
 
-        getData();
+        FirebaseUser currentPlayer = FirebaseAuth.getInstance().getCurrentUser();
+        playerKey = currentPlayer.getUid();
+
+
+        recyclerView = findViewById(R.id.rvRip);
+        layoutManager = new LinearLayoutManager(getBaseContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        playerRipList = new ArrayList<>();
+
 
         playerDB.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                totPlayerNum = Integer.parseInt(String.valueOf(snapshot.getChildrenCount())) - 1; //except instructor
+                playerRipList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    DB_Player playerInfo = null;
+                    playerInfo = dataSnapshot.getValue(DB_Player.class);
+                    if (playerInfo.playerRip.length() != 0){
+                        playerRipList.add(playerInfo);
+                    }
+                }
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -64,127 +88,28 @@ public class FinalpollResultActivity extends AppCompatActivity {
             }
         });
 
+        adapter = new RipAdapter(playerRipList, getBaseContext());
+        recyclerView.setAdapter(adapter);
+
+        //show all rip
+        adapter.setOnItemClickListener(new RipAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                selectRip = playerRipList.get(position);
+            }
+        });
+
+
+        // comment to player db
         btnSummitCom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //db update player comment
-               // playerDB.child("comment").setValue()
+               String comment = finalComment.getText().toString();
+               playerDB.child(playerKey).child("comment").setValue(comment);
+                Toast.makeText(FinalpollResultActivity.this, "Comment Submitted", Toast.LENGTH_SHORT).show();
+                finalComment.setText("");
             }
         });
-
-
-
-    }
-
-    //load Data from firebase
-    public void getData() {
-
-        //load main claim
-        mainClaimDB.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                DB_MainClaim mainClaim = null;
-                for (DataSnapshot child : snapshot.getChildren()) {
-                    mainClaim = child.getValue(DB_MainClaim.class);
-                }
-                entries.add(new BarEntry(1.0f, mainClaim.numStrawCon));
-                entries.add(new BarEntry(2.0f, mainClaim.numStrawNot));
-
-                BarDataSet barDataSet = new BarDataSet(getBarEntriesZero(), "Number of Student");
-                BarDataSet barDataSet1 = new BarDataSet(getBarEntriesOne(), "Newly Convinced");
-                BarDataSet barDataSet2 = new BarDataSet(getBarEntriesTwo(), "No Longer Persuaded");
-
-                initBarDataSet(barDataSet);
-                barDataSet.setColor(Color.parseColor("#BF360C"));
-                initBarDataSet(barDataSet1);
-                barDataSet1.setColor(Color.rgb(104,241,175));
-                initBarDataSet(barDataSet2);
-                barDataSet2.setColor(Color.rgb(164,221,251));
-
-                BarData data = new BarData(barDataSet,barDataSet1,barDataSet2);
-                data.setBarWidth(0.3f);
-                barChart.setData(data);
-                float groupSpace = 1.3f;
-                float barSpace = 0.05f;
-                float barWidth = 0.2f;
-
-
-                YAxis leftAxis = barChart.getAxisLeft();
-                leftAxis.setDrawGridLines(false);
-                leftAxis.setSpaceTop(30f);
-                barChart.getAxisRight().setEnabled(false);
-                barChart.getBarData().setBarWidth(barWidth);
-                barChart.groupBars(0, groupSpace, barSpace);
-                barChart.invalidate();
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-
-    public void initBarDataSet(BarDataSet barDataSet){
-        //Changing the color of the bar
-        barDataSet.setColor(Color.parseColor("#BF360C"));
-        //Setting the size of the form in the legend
-        barDataSet.setFormSize(8f);
-        //showing the value of the bar, default true if not set
-        barDataSet.setDrawValues(false);
-        //setting the text size of the value of the bar
-//        barDataSet.setValueTextSize(15f);
-        //remove the description label text located at the lower right corner
-
-        //setting yaxis
-        YAxis left = barChart.getAxisLeft();
-        left.setAxisMinimum(0f);
-        Description description = new Description();
-        description.setEnabled(false);
-        barChart.setDescription(description);
-        //hide labels of the axis
-        barChart.getXAxis().setDrawLabels(false);
-
-        //setting animation for y-axis, the bar will pop up from 0 to its value within the time we set
-        barChart.animateY(1000);
-    }
-
-    // array list for zero set.
-    private ArrayList<BarEntry> getBarEntriesZero() {
-
-        // creating a new array list
-        barEntries = new ArrayList<>();
-
-        // adding new entry to our array list with bar
-        // entry and passing x and y axis value to it.
-        barEntries.add(new BarEntry(1f, 8));
-        return barEntries;
-    }
-
-    // array list for first set
-    private ArrayList<BarEntry> getBarEntriesOne() {
-
-        // creating a new array list
-        barEntries = new ArrayList<>();
-
-        // adding new entry to our array list with bar
-        // entry and passing x and y axis value to it.
-        barEntries.add(new BarEntry(1f, 5));
-        return barEntries;
-    }
-
-    // array list for second set.
-    private ArrayList<BarEntry> getBarEntriesTwo() {
-
-        // creating a new array list
-        barEntries = new ArrayList<>();
-
-        // adding new entry to our array list with bar
-        // entry and passing x and y axis value to it.
-        barEntries.add(new BarEntry(1f, 3));
-        return barEntries;
     }
 }
